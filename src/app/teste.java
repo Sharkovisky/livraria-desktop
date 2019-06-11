69b6a4dbc0334c4123ccca81f632d583beeb3ba4
Sharkovisky <whiteaxefun@gmail.com>
2019-06-11 19:43:29 -0400

    Adicionar

--- src/app/teste.java
+++ src/app/teste.java
@@ -1,4 +1,23 @@
 package app;
 
+import dao.AutorDAO;
+import model.Autor;
+
 public class teste {
+
+    public static void main(String[] args) {
+
+        Autor a1 = new Autor();
+
+        AutorDAO a1DAO = new AutorDAO();
+
+        a1.setNome("Francicano Aeho");
+        a1.setEmail("franciscano.aeho@gmail.com");
+
+        //a1DAO.inserir(a1);
+
+        a1.setId(15);
+
+        a1DAO.deletar(a1);
+    }
 }
