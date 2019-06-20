c300967fc806084c348d5852b2561add68310354
Sharkovisky <whiteaxefun@gmail.com>
2019-06-20 00:39:35 -0400

    Principal e link

--- src/controller/PrincipalController.java
+++ src/controller/PrincipalController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.MenuItem;
+
+import java.io.IOException;
+
 public class PrincipalController {
+
+    @FXML private MenuItem menuItemCadastrar;
+
+    public void menuCadastrar(){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("autor_formulario.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try{
+            fxmlLoader.load();
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
